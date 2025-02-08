1ea43bf9ad0fd213f90b07ce166499b6602e187b
Jfmm1983 <123413274+Jfmm1983@users.noreply.github.com>
2025-02-08 22:30:56 +0000

    fix/proyecto_cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+let numberCartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let cartaBarajada = Math.floor(Math.random()*numberCartas.length);
+let elemento = document.getElementById("contenido");
+elemento.innerHTML = numberCartas[cartaBarajada];
 
+let palos = ["Espada", "Diamante", "Corazon", "Trebol"];
+let randomPaloNumero = Math.floor(Math.random() * palos.length);
+let paloSeleccionado = palos[randomPaloNumero];
+console.log(paloSeleccionado)
+document.getElementById('carta').classList.add(getCard(paloSeleccionado));
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getCard(palos) {
+    switch (palos) {
+      case "Diamante": return "diamante";
+      case "Espada": return "espada";
+      case "Corazon": return "corazon";
+      case "Trebol": return "trebol";
+    }
+}
